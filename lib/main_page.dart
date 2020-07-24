c7e06e7875e27139424c40ddea820e8aa554f6f1
Sungshik Kim <kimss@rsupport.com>
2020-07-25 06:33:10 +0900

    234

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -41,7 +41,7 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
         appBar: AppBar(
           // 2-4. 메인화면 - title 수정
-          title: Text('Movie'),
+          title: Text(_getMenuTitle(_sortIndex)),
           leading: Icon(Icons.menu),
           actions: <Widget>[
             // 2-4. 메인화면 - 팝업 메뉴 호출 함수화 (호출)
@@ -92,7 +92,8 @@ class _MainPageState extends State<MainPage> {
   }
 
   // 2-4. 메인화면 - 팝업 메뉴 호출 함수화 (선언)
-  _buildPopupMenuButton() => PopupMenuButton<int>(
+  _buildPopupMenuButton() =>
+      PopupMenuButton<int>(
         icon: Icon(Icons.sort),
         onSelected: _onTabSortMethod,
         itemBuilder: (context) {
@@ -111,20 +112,22 @@ class _MainPageState extends State<MainPage> {
     });
     _requestMovies();
   }
+
 // 2-4. 메인화면 - 각 index 에 맞는 제목을 호출해주는 로직 작성
+  _getMenuTitle(int index) => ['예매율', '큐레이션', '최신순'][index];
 
 // 2-2. 메인화면 - _buildPage() 함수 내용 수정
-  _buildPage(index, MoviesResponse moviesResponse) {
-    if (moviesResponse == null) {
-      return Center(child: CircularProgressIndicator());
-    }
-    switch (index) {
-      case 0:
-        return ListPage(moviesResponse.movies);
-      case 1:
-        return GridPage(moviesResponse.movies);
-      default:
-        return Container();
+    _buildPage(index, MoviesResponse moviesResponse) {
+      if (moviesResponse == null) {
+        return Center(child: CircularProgressIndicator());
+      }
+      switch (index) {
+        case 0:
+          return ListPage(moviesResponse.movies);
+        case 1:
+          return GridPage(moviesResponse.movies);
+        default:
+          return Container();
+      }
     }
   }
-}
